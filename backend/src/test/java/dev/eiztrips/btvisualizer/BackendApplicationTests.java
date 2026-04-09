b756edce90afcdc9467a328030b39d13c48d23b0
Eiztrips <Eiztrips@gmail.com>
2026-04-09 23:49:39 +0300

    Initial commit

---
+++ backend/src/test/java/dev/eiztrips/btvisualizer/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eiztrips.btvisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
